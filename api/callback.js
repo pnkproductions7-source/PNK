0513121fd1aa4f5435834594db31cd3534f0aa4d
pnkproductions7-source <pnkproductions7@gmail.com>
2026-02-11 17:56:44 +0200

    added github

---
+++ api/callback.js
@@ -0,0 +1,46 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+
+  try {
+    const response = await fetch(
+      "https://github.com/login/oauth/access_token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          client_id: GITHUB_CLIENT_ID,
+          client_secret: GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      },
+    );
+
+    const data = await response.json();
+    const content = {
+      token: data.access_token,
+      provider: "github",
+    };
+
+    // This script sends the token back to the CMS window and closes the popup
+    res.send(`
+      <script>
+        (function() {
+          function recieveMessage(e) {
+            window.opener.postMessage(
+              'authorization:github:success:${JSON.stringify(content)}',
+              e.origin
+            );
+          }
+          window.addEventListener("message", recieveMessage, false);
+          window.opener.postMessage("authorizing:github", "*");
+        })()
+      </script>
+    `);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
