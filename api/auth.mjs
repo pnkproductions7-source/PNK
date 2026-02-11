4d3c9ac5395128ede67ec9ba7ef6e7243903ac2a
pnkproductions7-source <pnkproductions7@gmail.com>
2026-02-11 23:34:13 +0200

    fixes

--- api/auth.mjs
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-  // Redirects the user to GitHub to authorize the app
-  const url = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=repo,user`;
-  res.redirect(url);
-}
